18c96ec77fa7da5fddee06f2db9e8b69e8021b35
Emily Huggins <emilyhuggins@btinternet.com>
2025-02-02 21:39:45 +0000

    Functioning API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
-fetch("https://cataas.com/")
-.then(response => {
-    if (!response.ok) {
-        throw new Error("Network response was not ok");
-}
-    return response.blob();
-})
 
-.then(data => console.log(data))
-.catch(error => console.error(error));
+function getRandomCat() {
+    fetch("https://cataas.com/cat?json=true")
+        .then(response => {
+            if (!response.ok) {
+                console.log("Response not ok, throwing error");
+                throw new Error("Network response was not OK");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("API response:", data); // Log the entire response
+            if (data._id) {
+                document.getElementById("cat-pic").src = "https://cataas.com/cat/" + data._id;
+                document.getElementById("cat-pic").style.display = 'block';
+            } else {
+                console.error("URL not found in the response");
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching cat image:", error);
+        });
+}

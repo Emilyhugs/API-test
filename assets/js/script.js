70dc3d858a2279b9e985f8cb5acbe3408f7ed947
Emily Huggins <emilyhuggins@btinternet.com>
2025-02-03 11:38:59 +0000

    test pause

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,43 @@
 
-function getRandomCat() {
-    fetch("https://cataas.com/cat?json=true")
-        .then(response => {
-            if (!response.ok) {
-                console.log("Response not ok, throwing error");
-                throw new Error("Network response was not OK");
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log("API response:", data); // Log the entire response
-            if (data._id) {
-                document.getElementById("cat-pic").src = "https://cataas.com/cat/" + data._id;
-                document.getElementById("cat-pic").style.display = 'block';
-            } else {
-                console.error("URL not found in the response");
-            }
-        })
-        .catch(error => {
-            console.error("Error fetching cat image:", error);
-        });
+// function getRandomCat() {
+//     fetch("https://cataas.com/cat?json=true")
+//         .then(response => {
+//             if (!response.ok) {
+//                 console.log("Response not ok, throwing error");
+//                 throw new Error("Network response was not OK");
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             console.log("API response:", data); // Log the entire response
+//             if (data._id) {
+//                 document.getElementById("cat-pic").src = "https://cataas.com/cat/" + data._id;
+//                 document.getElementById("cat-pic").style.display = 'block';
+//             } else {
+//                 console.error("URL not found in the response");
+//             }
+//         })
+//         .catch(error => {
+//             console.error("Error fetching cat image:", error);
+//         });
+// }
+
+async function getRandomCat() {
+    try {
+        const response = await fetch("https://cataas.com/cat?json=true");
+        if (!response.ok) {
+            console.log("Response not ok, throwing error");
+            throw new Error("Network response was not OK");
+        }
+        const data = await response.json();
+        console.log("API response:", data); // Log the entire response
+        if (data._id) {
+            document.getElementById("cat-pic").src = "https://cataas.com/cat/" + data._id;
+            document.getElementById("cat-pic").style.display = 'block';
+        } else {
+            console.error("URL not found in the response");
+        }
+    } catch (error) {
+        console.error("Error fetching cat image:", error);
+    }
 }
